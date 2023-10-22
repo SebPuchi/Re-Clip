a806f0803f926b2620a0d208b4eb940dc1e21e2a
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-10-22 04:47:32 -0400

    localsend

--- Front-End/TikTok Clone (SwiftUI)/Model/AIVideoModels.swift
+++ Front-End/TikTok Clone (SwiftUI)/Model/AIVideoModels.swift
@@ -15,7 +15,7 @@ struct AIVideoModels: Identifiable {
 
 extension AIVideoModels {
     static let videos: [AIVideoModels] = [
-        AIVideoModels(videoURL: Bundle.main.url(forResource: "Snaptik", withExtension: "mp4")!),
+        AIVideoModels(videoURL: Bundle.main.url(forResource: "localsend", withExtension: "mp4")!),
         AIVideoModels(videoURL: Bundle.main.url(forResource: "LLM.", withExtension: "mp4")!),
         AIVideoModels(videoURL: Bundle.main.url(forResource: "open.pilot", withExtension: "mp4")!)
     ]
