6d580f9f2dad61b27e9fe5e4bf97b0019b447e01
Andrew Boessen <boessena@bc.edu>
2023-10-21 20:25:10 -0400

    add more images

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ import os
 
 
 pipeline = AutoPipelineForText2Image.from_pretrained(
-	"stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float32
+	"kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float32
 ).to()
 
 image = pipeline(
-	prompt="Human like robots playing soccer against each other on a soccer field, action, detailed, photo", 
-	negative_prompt="low quality, bad quality, bad anatomy", 
+	prompt="In this image, a designer is depicted at their work desk, carefully crafting a digital interface. The background shows a rapidly changing technological landscape. The designer appears deep in thought, reflecting the authors' call for mindfulness in practicing ethical sensibilities in day-to-day design work, reaslistic, detailed, 8k", 
+	negative_prompt="low quality, bad quality, neon colors, bad anatomy", 
 	guidance_scale=7.5,
-	height=780,
+	height=768,
 	width=360,
 ).images[0]
 
-image.save("robotSDXL.png")
+image.save("designer.png")
